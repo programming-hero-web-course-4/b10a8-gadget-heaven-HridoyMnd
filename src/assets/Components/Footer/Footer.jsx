37e299087b8a023c84ecc2afc558b87057183eff
Hridoy Mnd <hridoymnd10@gmail.com>
2024-11-03 20:26:23 +0600

    footer section design done

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -1,9 +1,36 @@
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer Component</h2>
-        </div>
+        <section className="container mx-auto">
+            <footer>
+                <div className="mb-10 text-center">
+                    <h1 className="font-bold text-3xl mb-3">Gadget Heaven</h1>
+                    <p className="font-semibold text-gray-600">Leading the way in cutting-edge technology and innovation.</p>
+                </div>
+                <hr />
+                <div className="flex justify-between mt-4">
+                    <div>
+                        <h3 className="font-bold text-lg mb-3">Services</h3>
+                        <p className="font-medium text-gray-600">Product Support</p>
+                        <p className="font-medium text-gray-600">Order Tracking</p>
+                        <p className="font-medium text-gray-600">Shipping Delivery</p>
+                        <p className="font-medium text-gray-600">Returns</p>
+                    </div>
+                    <div>
+                        <h3 className="font-bold text-lg mb-3">Company</h3>
+                        <p className="font-medium text-gray-600">About Us</p>
+                        <p className="font-medium text-gray-600">Careers</p>
+                        <p className="font-medium text-gray-600">Contact</p>
+                    </div>
+                    <div>
+                        <h3 className="font-bold text-lg mb-3">Legal</h3>
+                        <p className="font-medium text-gray-600">Terms of Service</p>
+                        <p className="font-medium text-gray-600">Privacy Policy</p>
+                        <p className="font-medium text-gray-600">Cookie Policy</p>
+                    </div>
+                </div>
+            </footer>
+        </section>
     );
 };
 
