ac7419870b7aae33680668f4969b7fc8299e48aa
Hridoy Mnd <hridoymnd10@gmail.com>
2024-11-03 20:39:21 +0600

    recheck code format and something change

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -3,12 +3,16 @@ const Footer = () => {
     return (
         <section className="container mx-auto">
             <footer>
+                {/* heading part */}
                 <div className="mb-10 text-center">
                     <h1 className="font-bold text-3xl mb-3">Gadget Heaven</h1>
                     <p className="font-semibold text-gray-600">Leading the way in cutting-edge technology and innovation.</p>
                 </div>
+                {/* horizontal rule */}
                 <hr />
+                {/* bottom part */}
                 <div className="flex justify-between mt-4">
+                    {/* left part */}
                     <div>
                         <h3 className="font-bold text-lg mb-3">Services</h3>
                         <p className="font-medium text-gray-600">Product Support</p>
@@ -16,12 +20,14 @@ const Footer = () => {
                         <p className="font-medium text-gray-600">Shipping Delivery</p>
                         <p className="font-medium text-gray-600">Returns</p>
                     </div>
+                    {/* center part */}
                     <div>
                         <h3 className="font-bold text-lg mb-3">Company</h3>
                         <p className="font-medium text-gray-600">About Us</p>
                         <p className="font-medium text-gray-600">Careers</p>
                         <p className="font-medium text-gray-600">Contact</p>
                     </div>
+                    {/* right part */}
                     <div>
                         <h3 className="font-bold text-lg mb-3">Legal</h3>
                         <p className="font-medium text-gray-600">Terms of Service</p>
--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -3,11 +3,13 @@ import { LuShoppingCart } from "react-icons/lu";
 import { FaRegHeart } from "react-icons/fa";
 const Navbar = () => {
     return (
-        <div className="border border-sky-400 container mx-auto">
+        <section className="container mx-auto">
             <nav className="flex justify-between items-center">
+                {/* nav logo part */}
                 <div>
                     <h1 className="text-xl font-bold">Gadget Heaven</h1>
                 </div>
+                {/* nav menu part */}
                 <div>
                     <ul className="flex gap-2">
                         <Link className="px-5 py-2 font-medium text-gray-600">Home</Link>
@@ -15,12 +17,14 @@ const Navbar = () => {
                         <Link className="px-5 py-2 font-medium text-gray-600">Dashboard</Link>
                     </ul>
                 </div>
+                {/* nav button part */}
                 <div className="flex gap-4">
                     <button className="border h-10 w-10 rounded-full flex justify-center items-center"><LuShoppingCart className="" /></button>
                     <button className="border h-10 w-10 rounded-full flex justify-center items-center"><FaRegHeart className="" /></button>
                 </div>
             </nav>
-        </div>
+        </section>
+
     );
 };
 
